72a463537ec7b18c401745f7aa7930d2e2368aa4
Alwyn-Tan <2660412180@qq.com>
2025-03-23 17:18:27 +0800

    upgrade: Etch a sketch now can gracefully change color and erase color, but can not resize

--- Etch-A-Sketch/script.js
+++ Etch-A-Sketch/script.js
@@ -1,33 +1,88 @@
-const gridContainer = document.querySelector(".grid-container")
 const body = document.querySelector("body")
-const menu = document.querySelector(".menu")
+const colorSelector = document.querySelector(".color-selector")
+const colorModeBtn = document.querySelector(".color-mode-btn")
+const rainbowModeBtn = document.querySelector(".rainbow-mode-btn")
+const eraserBtn = document.querySelector(".eraser-btn")
 const clearBtn = document.querySelector(".clear-btn")
+const sizeValue = document.querySelector(".size-value")
+const sizeSlider = document.querySelector(".size-slider")
+const gridContainer = document.querySelector(".grid-container")
 
-let size = 16
-const flexGrids = []
+DEFAULT_COLOR = '#333333'
+DEFAULT_MODE = 'color'
+DEFAULT_SIZE = 16
+
+let currentColor = DEFAULT_COLOR
+let currentMode = DEFAULT_MODE
+let currentSize = DEFAULT_SIZE
+
+colorSelector.oninput = (e) => setCurrentColor(e.target.value)
+colorModeBtn.onclick = () => setCurrentMode('color')
+rainbowModeBtn.onclick = () => setCurrentMode('rainbow')
+eraserBtn.onclick = () => setCurrentMode('eraser')
+clearBtn.onclick = () => clearGrid()
+sizeSlider.onmousemove = (e) => changeSize(e.target.value)
+sizeSlider.onchange = (e) => changeSize(e.target.value)
+
+let mouseDown = false
+body.onmousedown = () => (mouseDown = true)
+body.onmouseup = () => (mouseDown = false)
+
+let flexGrids = []
 var gridContainerStyle = window.getComputedStyle(gridContainer)
 var gridContainerWidth = parseFloat(gridContainerStyle.width)
-function setSize(_size) {
-    size = _size
+loadGrid()
+
+function setCurrentColor(_color) {
+    currentColor = _color
 }
 
-for (let i = 0; i < size * size; i++) {
-    const flexGrid = document.createElement('div')
-    flexGrid.style.width = gridContainerWidth / size + "px"
-    flexGrid.style.height = flexGrid.style.width
-    flexGrid.addEventListener('click', () => {
-        flexGrid.style.background = 'red'
-    })
-
-    gridContainer.appendChild(flexGrid)
-    flexGrids.push(flexGrid)
+function setCurrentMode(_mode) {
+    currentMode = _mode
 }
 
-clearBtn.addEventListener('click', () => {
+function setCurrentSize(_size) {
+    currentSize = _size
+}
+
+function changeSize(_size) {
+    setCurrentSize(_size)
+    sizeValue.innerHTML = `${_size} x ${_size}`
+    loadGrid()
+}
+
+function clearGrid() {
     flexGrids.forEach(flexGrid => {
-        flexGrid.style.background = 'white'
+        flexGrid.style.backgroundColor = '#fefefe'
     });
-})
+}
+
+function loadGrid() {
+    for (let i = 0; i < currentSize * currentSize; i++) {
+        const flexGrid = document.createElement('div')
+        flexGrid.style.width = gridContainerWidth / currentSize + "px"
+        flexGrid.style.height = flexGrid.style.width
+        flexGrid.addEventListener('mouseover', changeColor)
+        flexGrid.addEventListener('mousedown', changeColor)
+        gridContainer.appendChild(flexGrid)
+        flexGrids.push(flexGrid)
+    }
+}
+
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    if (currentMode === 'color') {
+        e.target.style.backgroundColor = currentColor
+    } else if (currentMode === 'rainbow') {
+        const randomR = Math.floor(Math.random() * 256)
+        const randomG = Math.floor(Math.random() * 256)
+        const randomB = Math.floor(Math.random() * 256)
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if (currentMode === 'eraser') {
+        e.target.style.backgroundColor = '#fefefe'
+    }
+}
+
 
 
 
