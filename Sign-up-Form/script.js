53b0d6c3a28235df851455528c9f44ccaa0d221a
Alwyn-Tan <2660412180@qq.com>
2025-05-26 21:31:05 +0800

    add:sign-up form blur filter

--- Sign-up-Form/script.js
+++ Sign-up-Form/script.js
@@ -1,15 +1,12 @@
-const main = document.querySelector('main');
-const blur = document.querySelector('.blur');
-
-
 function setBlurMask() {
-    const mainSize = main.getBoundingClientRect()
-    const blurSize = blur.getBoundingClientRect()
-    const topPercent = (mainSize.top / blurSize.height) * 100 + "%";
-    const bottomPercent = (mainSize.bottom / blurSize.height) * 100 + "%";
-    const leftPercent = (mainSize.left / blurSize.width) * 100 + "%";
-    const rightPercent = (mainSize.right / blurSize.width) * 100 + "%";
-
+    const main = document.querySelector("main");
+    const size = main.getBoundingClientRect();
+    const blur = document.querySelector(".blur");
+    const blurSize = blur.getBoundingClientRect();
+    const topPercent = (size.top / blurSize.height) * 100 + "%";
+    const leftPercent = (size.left / blurSize.width) * 100 + "%";
+    const bottomPercent = (size.bottom / blurSize.height) * 100 + "%";
+    const rightPercent = (size.right / blurSize.width) * 100 + "%";
     document
         .querySelector(":root")
         .style.setProperty(
@@ -17,3 +14,11 @@ function setBlurMask() {
         `polygon(0% 0%, 0% 100%, ${leftPercent} 100%, ${leftPercent} ${topPercent}, ${rightPercent} ${topPercent}, ${rightPercent} ${bottomPercent}, ${leftPercent} ${bottomPercent}, ${leftPercent} 100%, 100% 100%, 100% 0%)`
     );
 }
+
+window.addEventListener("resize", () => {
+    setBlurMask();
+})
+
+window.addEventListener("load", () => {
+    setBlurMask();
+})
